2495aa34fe9243068a4a510637340220123d4dfe
zhihanii <673084472@qq.com>
2022-10-16 19:36:30 +0800

    update

--- options.go
+++ options.go
@@ -4,12 +4,19 @@ type Option func(o *options)
 
 type options struct {
 	interceptor      ServerInterceptor
+	chainInts        []ServerInterceptor
 	numServerWorkers uint32
 }
 
 func Interceptor(i ServerInterceptor) Option {
 	return func(o *options) {
 		o.interceptor = i
+	}
+}
+
+func ChainInterceptor(ints ...ServerInterceptor) Option {
+	return func(o *options) {
+		o.chainInts = append(o.chainInts, ints...)
 	}
 }
 
